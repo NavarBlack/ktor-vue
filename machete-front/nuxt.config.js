ff5785f392da82e31e89c9f6c0ce9db140bf26a2
Dima.D <ivanbezdomni@yandex.ru>
2021-06-29 15:26:05 +0300

    MCH-001
    Проект запущен

---
+++ machete-front/nuxt.config.js
@@ -0,0 +1,60 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Стена-душегубка',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['@assets/css/common.css'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: false,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    '@nuxtjs/style-resources',
+  ],
+  styleResources: {
+    sass: ['./assets/sass/*.sass'],
+  },
+
+  axios: {
+    proxy: true,
+  },
+
+  proxy: {
+    '/api/v1': '0.0.0.0:9000',
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'ru',
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+
+  server: {
+    host: '0.0.0.0',
+    port: '3333',
+  },
+};
